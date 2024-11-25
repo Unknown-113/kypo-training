70d9accba295dcb48ae3227f7da3bbeffe3518aa
majdan-cshub <445519@muni.cz>
2024-11-25 02:24:27 +0100

    Update access level DTOs with lombok

--- kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/accesslevel/ValidatePasskeyDTO.java
+++ kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/accesslevel/ValidatePasskeyDTO.java
@@ -3,7 +3,11 @@ package cz.muni.ics.kypo.training.api.dto.accesslevel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ValidatePasskeyDTO {
     @ApiModelProperty(value = "Passkey to be validated.", required = true, example = "passkey")
     @NotEmpty(message = "{passkeyToValidate.passkey.NotEmpty.message}")
