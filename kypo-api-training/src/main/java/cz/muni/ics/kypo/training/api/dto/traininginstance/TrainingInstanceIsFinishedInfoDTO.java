26db9dc816b2280468c47ad69b6084db95ca2661
majdan-cshub <445519@muni.cz>
2024-11-30 05:45:00 +0100

    Restore tr folder

--- kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/traininginstance/TrainingInstanceIsFinishedInfoDTO.java
+++ kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/traininginstance/TrainingInstanceIsFinishedInfoDTO.java
@@ -2,14 +2,10 @@ package cz.muni.ics.kypo.training.api.dto.traininginstance;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
 
 /**
  * Encapsulates the information stating if training instance has finished.
  */
-@Getter
-@Setter
-@ToString
 @ApiModel(value = "TrainingInstanceIsFinishedInfoDTO", description = "Information stating if training instance has finished.")
 public class TrainingInstanceIsFinishedInfoDTO {
 
@@ -17,4 +13,48 @@ public class TrainingInstanceIsFinishedInfoDTO {
     private boolean hasFinished;
     @ApiModelProperty(value = "Message about training instance state.", example = "false")
     private String message;
+
+    /**
+     * Get if instance has finished.
+     *
+     * @return the boolean
+     */
+    public boolean getHasFinished() {
+        return hasFinished;
+    }
+
+    /**
+     * Sets if instance has finished.
+     *
+     * @param hasFinished the has finished
+     */
+    public void setHasFinished(boolean hasFinished) {
+        this.hasFinished = hasFinished;
+    }
+
+    /**
+     * Gets message.
+     *
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets message.
+     *
+     * @param message the message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainingInstanceIsFinishedInfoDTO{" +
+                "hasFinished=" + hasFinished +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
--- kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/traininglevel/TrainingLevelPreviewDTO.java
+++ kypo-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/traininglevel/TrainingLevelPreviewDTO.java
@@ -7,14 +7,12 @@ import io.swagger.annotations.ApiModelProperty;
 
 import java.util.HashSet;
 import java.util.Set;
-import lombok.*;
+
 
 /**
  * Encapsulates basic information about training level.
  */
-@Getter
-@Setter
-@ToString
+
 @ApiModel(value = "TrainingLevelPreviewDTO", description = "An assignment containing security tasks whose completion yields a answer.", parent = AbstractLevelDTO.class)
 public class TrainingLevelPreviewDTO extends AbstractLevelDTO {
 
@@ -24,4 +22,40 @@ public class TrainingLevelPreviewDTO extends AbstractLevelDTO {
     private Set<TakenHintDTO> hints = new HashSet<>();
     @ApiModelProperty(value = "Instruction how to get answer in training.", example = "This is how you do it")
     private String solution;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Set<TakenHintDTO> getHints() {
+        return hints;
+    }
+
+    public void setHints(Set<TakenHintDTO> hints) {
+        this.hints = hints;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainingLevelPreviewDTO{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", levelType=" + levelType +
+                ", order=" + order +
+                ", content='" + content + '\'' +
+                ", solution='" + solution + '\'' +
+                '}';
+    }
 }
